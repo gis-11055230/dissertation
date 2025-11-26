c0744bfc694362eaa320e8929e89e323efb7af2d
gis-11055230 <eve.jankowski@student.manchester.ac.uk>
2025-11-26 22:56:39 +0000

    extracted pop centroids

--- dissertation.py
+++ dissertation.py
@@ -1,6 +1,7 @@
 # IMPORT DATA
+
 import pandas as pd
-import csv
+
 
 # FUNCTIONS
 
@@ -14,10 +15,15 @@ gm_trse = pd.read_csv("trse_data.csv")
 # extract the data of the 10% worst trse areas
 worst_10 = gm_trse[gm_trse["eng_trse_10pct"] == True]
 
-print(len(worst_10))
-
-# SPATIAL INDEX
+print("10% worst TRSE areas:", len(worst_10))
 
 
-# PLOTTING MAP
+# read population points from csv file
+pop_points = pd.read_csv("pop_points_centroids.csv")
+
+
+# extract the population points out of the 10% worst gm OA's
+worst_pop_points = pop_points[pop_points["OA21CD"].isin(worst_10["name"])]
+
+print("Population points in worst 10% areas:", len(worst_pop_points))
 
